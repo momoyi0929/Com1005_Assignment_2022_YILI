2671d744418efbc5cbac9c2c054638e7ccf86582
momoyi0929 <103149151+momoyi0929@users.noreply.github.com>
2022-04-25 22:02:38 +0100

    Update EpuzzleSearch.java
    fixed some format problem

--- BFS/EpuzzleSearch.java
+++ BFS/EpuzzleSearch.java
@@ -1,20 +1,20 @@
 public class EpuzzleSearch extends Search {
-	private int[] goal;
-	private final int size = 9;
-	public EpuzzleSearch(int[]goal){
-		this.goal = new int[goal.length];
-		for(int i=0; i<goal.length; i++) {
-			this.goal[i] = goal[i];
-		}
+    private int[] goal;
+    private final int size = 9;
+    public EpuzzleSearch(int[]goal){
+	this.goal = new int[goal.length];
+	for(int i=0; i<goal.length; i++) {
+	    this.goal[i] = goal[i];
 	}
+    }
 	
-	public int getSize() {
-		return size;
-	}
+    public int getSize() {
+	return size;
+    }
 	
-	public int[] getGoal(){
-		return goal;
-	}
+    public int[] getGoal(){
+	return goal;
+    }
 	
 	
 }
