be913fc30a3eaaf6dfaca50a0e29765bffa8bedc
momoyi0929 <103149151+momoyi0929@users.noreply.github.com>
2022-04-29 19:36:18 +0100

    Add files via upload

---
+++ ASTAR/EpuzzleSearch.java
@@ -0,0 +1,3 @@
+public class EpuzzleSearch extends Search {
+	
+}
---
+++ ASTAR/EpuzzleState.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class EpuzzleState extends SearchState{
+
+	@Override
+	boolean goalPredicate(Search searcher) {
+		
+		return false;
+	}
+
+	@Override
+	ArrayList<SearchState> getSuccessors(Search searcher) {
+		
+		return null;
+	}
+
+	@Override
+	boolean sameState(SearchState n2) {
+		
+		return false;
+	}
+	
+}
