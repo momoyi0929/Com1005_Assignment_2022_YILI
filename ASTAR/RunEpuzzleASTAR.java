e41c63c889be6539a64d9ceea8a4e361f2a09688
momoyi0929 <103149151+momoyi0929@users.noreply.github.com>
2022-04-30 14:32:40 +0100

    Update RunEpuzzleASTAR.java

--- ASTAR/RunEpuzzleASTAR.java
+++ ASTAR/RunEpuzzleASTAR.java
@@ -1,3 +1,10 @@
 public class RunEpuzzleASTAR {
+	// generator given seed
+	int seed = 23456;
+	EpuzzGen gen = new EpuzzGen(seed);
+	
+	// generate puzzle providing difficulty
+	int d = 6;
+	int[][] puzz = gen.puzzGen(d);
 	
 }
