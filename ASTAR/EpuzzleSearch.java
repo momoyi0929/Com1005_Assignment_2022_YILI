fa7a17e1e61c4bd5fa032565df7d4c116c8d7904
momoyi0929 <103149151+momoyi0929@users.noreply.github.com>
2022-04-30 16:37:57 +0100

    Update EpuzzleSearch.java

--- ASTAR/EpuzzleSearch.java
+++ ASTAR/EpuzzleSearch.java
@@ -1,3 +1,18 @@
 public class EpuzzleSearch extends Search {
+	private int[][] puzz;
+	private int[][] goal;
+	
+	EpuzzleSearch(int[][]puzz,int[][]goal){
+		this.puzz=puzz;
+		this.goal=goal;
+	}
+	
+	public int[][] getPuzz(){
+		return puzz;
+	}
+	
+	public int[][] getGoal(){
+		return goal;
+	}
 	
 }
