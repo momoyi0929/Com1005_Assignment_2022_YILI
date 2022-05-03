4132d3a10520dbbcbb5a663e8c68d3f10e5bbba6
momoyi0929 <103149151+momoyi0929@users.noreply.github.com>
2022-05-03 22:25:46 +0100

    Update EpuzzleSearch.java

--- BFS/EpuzzleSearch.java
+++ BFS/EpuzzleSearch.java
@@ -1,20 +1,33 @@
+/**
+ * EpuzzleSearch extends Search
+ * @author YI LI
+ *
+ */
 public class EpuzzleSearch extends Search {
-    private int[] goal;
-    private final int size = 9;
-    public EpuzzleSearch(int[]goal){
-	this.goal = new int[goal.length];
-	for(int i=0; i<goal.length; i++) {
-	    this.goal[i] = goal[i];
+	private int[] goal;
+	private final int size = 9;
+	
+	/**
+	   * constructor
+	   * 
+	   * @param goal int[]
+	   */
+	public EpuzzleSearch(int[]goal){
+		this.goal = new int[goal.length];
+		for(int i=0; i<goal.length; i++) {
+			this.goal[i] = goal[i];
+		}
 	}
-    }
-	
-    public int getSize() {
-	return size;
-    }
-	
-    public int[] getGoal(){
-	return goal;
-    }
-	
-	
+	/**
+	   * accessor for size
+	   */
+	public int getSize() {
+		return size;
+	}
+	/**
+	   * accessor for goal
+	   */
+	public int[] getGoal(){
+		return goal;
+	}	
 }
